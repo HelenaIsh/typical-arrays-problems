26b772b70c7f7fa8284172eb0f2abbbaf07bc337
HelenaIsh <elena-ismatova@mail.ru>
2021-01-08 01:24:31 +0500

    feat: implement functions min, max and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length === 0) return 0;
+    let min = array[0];
+    for (const el of array) {
+        if (el < min) {
+            min = el;
+        }
+    }
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length === 0) return 0;
+    let max = array[0];
+    for (const el of array) {
+        if (el > max) {
+            max = el;
+        }
+    }
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length === 0) return 0;
+    let sum = array.reduce((sum, el) => (sum + el), 0);
+    return sum/array.length;
 }
